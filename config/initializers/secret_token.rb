a04e06c924cb98ba1ff6d7323dc75d781bb0f068
til <tilman.boerner@gmx.net>
2013-05-13 15:15:46 +0200

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Laserchicken::Application.config.secret_key_base = '8965cd8dc7f0572909624450480946b459a5c4b90e07ebe56b9207caaad249dd062df0fe0b5ad3a40e87a3f78c79965c1300990285237f7b29f978341eed5982'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Laserchicken::Application.config.session_store :cookie_store, key: '_laserchicken_session'
