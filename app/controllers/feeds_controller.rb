2acad72b295e1077581f9b85970be964c370f6de
til <tilman.boerner@gmx.net>
2013-06-19 14:25:57 +0200

    feeds resource with create and destroy

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,22 @@
 class FeedsController < ApplicationController
+	include FeedsHelper
+
+	def index
+		@feeds = Feed.all
+	end
+
+
+	def create
+		@feed = create_feed(params.require(:feed)[:feed_url])
+		@feed.save
+		redirect_to feeds_path
+	end
+
+
+	def destroy
+		@feed = Feed.find(params[:id])
+		@feed.destroy
+		redirect_to feeds_path
+	end
+
 end
--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,8 @@
 module FeedsHelper
+
+	def create_feed(url)
+		fz = Feedzirra::Feed.fetch_and_parse(url)
+		Feed.new fz.instance_values.slice 'title', 'url', 'feed_url', 'etag', 'last_modified'
+	end
+
 end
