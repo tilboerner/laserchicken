8b5fce5c6693feabc73821ec63692d7f3a3be353
til <tilman.boerner@gmx.net>
2013-07-24 17:54:13 +0200

    logging for feed updates

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -10,13 +10,12 @@ module FeedsHelper
 	def update_feed(feedmodel)
 		parser = model_to_parser(feedmodel)
 		Feedzirra::Feed.update(parser)
-		if parser.updated?
-			create_entries(feedmodel, parser.new_entries)
-			updatable_attributes = parser.instance_values.slice('title', 'url', 'etag', 'last_modified')
-			updatable_attributes.each do |name, value|
-				feedmodel.update_attribute(name, value)		# skip validation (!!!), which would http-get the feed again
-			end
-			feedmodel.subscriptions.each { |s| s.update_dependent_fields }
+		if parser.updated? or parser.has_new_entries?
+			logger.info "Updating feed #{feedmodel.id} '#{feedmodel.feed_url}'"
+			update_feed_from_parser(feedmodel, parser)
+			logger.info "done"
+		else
+			logger.debug "feed unchanged: #{feedmodel.id} '#{feedmodel.feed_url}'"
 		end
 		feedmodel
 	end
@@ -37,7 +36,27 @@ private
 		p
 	end
 
+	def update_feed_from_parser(feedmodel, parser)
+		update_model_attributes(feedmodel, parser) if parser.updated?
+		create_entries(feedmodel, parser.new_entries) if parser.has_new_entries?
+		feedmodel.subscriptions.each { |s| s.update_dependent_fields }
+	end
+
+	def update_model_attributes(model, parser)
+		updatable_attributes = %w(title url last_modified etag)
+		updatable_attributes.each do |name|
+			original_value = model.send name
+			new_value = parser.send name
+			unless new_value.nil? || new_value == original_value
+				logger.info "#{name}: #{original_value} -> #{new_value}"
+				model.update_attribute(name, new_value)		# skip validation (!!!), which would http-get the feed again
+			end
+		end
+	end
+
 	def create_entries(feedmodel, new_entries)
+		newcount = new_entries.size
+		logger.info "Processing #{newcount} new " + 'entry'.pluralize(newcount)
 		ActiveRecord::Base.transaction do
 			for e in new_entries
 				e.sanitize!
