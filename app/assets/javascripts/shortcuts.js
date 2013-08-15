06b5d65b9cb26eea9ba2c3ac3d2fd1c2b762e7c0
XORwell <XORwell@github.com>
2013-08-15 12:32:38 +0200

    use event.which (firefox support) or event.keyCode

--- app/assets/javascripts/shortcuts.js
+++ app/assets/javascripts/shortcuts.js
@@ -1,28 +1,37 @@
 $(function(){
     $(window).keypress(function(event){
-        // Overview
-        if(($('input:focus').length <= 0) && ($('article').length > 1) && (String.fromCharCode(event.keyCode) == 'A')) {
-            $("a[data-type='mark_all_seen']").click();
-        }
-        // Entry
-        if($('article').length == 1) {
-            switch (String.fromCharCode(event.keyCode)){
-                case 'm':
-                    $("a[data-type='read_unread']").click();
-                    break;
-                case 's':
-                    $("a[data-type='star_unstar']").click();
-                    break;
-                case 'j':
-                    Turbolinks.visit($("a[rel='next']").attr('href'));
-                    break;
-                case 'k':
-                    Turbolinks.visit($("a[rel='prev']").attr('href'));
-                    break;
-                case 'v':
-                    window.location.href = $("a[data-type='web']").attr('href');
-                    break;
+        var charCode = event.which || event.keyCode;
+        var key = String.fromCharCode(charCode);
+
+
+        if ($('input:focus').length <= 0){
+
+            // Overview
+            if(($('article').length > 1) && (key == 'A')) {
+                $("a[data-type='mark_all_seen']").click();
             }
+            // Entry
+            if($('article').length == 1) {
+                switch (key){
+                    case 'm':
+                        $("a[data-type='read_unread']").click();
+                        break;
+                    case 's':
+                        $("a[data-type='star_unstar']").click();
+                        break;
+                    case 'j':
+                        Turbolinks.visit($("a[rel='next']").attr('href'));
+                        break;
+                    case 'k':
+                        Turbolinks.visit($("a[rel='prev']").attr('href'));
+                        break;
+                    case 'v':
+                        window.location.href = $("a[data-type='web']").attr('href');
+                        break;
+                }
+            }
+
         }
+
     });
 });
